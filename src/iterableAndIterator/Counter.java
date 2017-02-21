62128963de69dc8b8dbeef7d6944d39f161f3604
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-02-21 22:39:44 +0700

    New Commit

--- src/iterableAndIterator/Counter.java
+++ src/iterableAndIterator/Counter.java
@@ -1,6 +1,26 @@
 package iterableAndIterator;
+import java.util.*;
+
 /**
  * Created by anonymous.vn1985 on 2/21/2017.
  */
-public class Counter {
+public class Counter implements Iterable<Integer>{
+    private int counter;
+    public Counter(int counter){
+        this.counter = counter;
+    }
+    public Iterator<Integer> iterator(){
+        return new Iterator<Integer>(){
+            int i;
+            public boolean hasNext(){
+                return i < counter;
+            }
+            public Integer next(){
+                return ++i;
+            }
+            public void remove(){
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
 }
