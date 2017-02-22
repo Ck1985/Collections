7df4e334b1c03bd3aba8d7775b83e8815a32afe0
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-02-22 23:43:21 +0700

    New Commit

---
+++ src/Task.java
@@ -0,0 +1,20 @@
+/**
+ * Created by anonymous.vn1985 on 2/22/2017.
+ */
+public abstract class Task implements Comparable<Task>{
+    protected Task(){}
+    public boolean equals(Object object){
+        if(object instanceof Task){
+            return this.toString().equals(object.toString());
+        }else{
+            return false;
+        }
+    }
+    public int compareTo(Task task){
+        return this.toString().compareTo(task.toString());
+    }
+    public int hashCode(){
+        return this.toString().hashCode();
+    }
+    public abstract String toString();
+}
